a1b634b6debfe1b5c14d9d39f96f8b75d790567c
Chip Hill <hillkarl03@gmail.com>
2022-10-15 04:57:00 -0400

    completed MVP of header and main page styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,15 +3,14 @@ import React from "react";
 const Header = () => {
 
   return (
-    <header>
-      <h2>Double Shot Rock Band</h2>
-      <ul className="d-flex flex-wrap justify-content-around">
-        <li>About</li>
-        <li>Events</li>
-        <li>The Music</li>
-        <li>Gallery</li>
+    <header className="d-flex flex-wrap flex-row justify-content around bg-black">
+      <ul id="header-nav" className="d-flex flex-wrap flex-row justify-content-evenly mx-auto p-2 w-50 text-light text-decoration-none">
+        <li className="mx-2 list-inline-item">About</li>
+        <li className="mx-2 list-inline-item">Events</li>
+        <li className="mx-2 list-inline-item">The Music</li>
+        <li className="mx-2 list-inline-item">Gallery</li>
         <span>
-          <li>Get in touch!!!</li>
+          <li className="nav-item list-inline-item">Get in touch!!!</li>
         </span>
       </ul>
     </header>
---
+++ src/components/Main.js
@@ -0,0 +1,16 @@
+import React from "react";
+import logo from "../img/ds-logo.jpg";
+
+const Main = () => {
+  return (
+    <div className="main-container bg-black">
+      <main>
+        <div className="ds-logo">
+       <center><img src={logo} alt="band logo"/></center> 
+       </div>
+      </main>
+    </div>
+  );
+};
+
+export default Main;
