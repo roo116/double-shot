0c0a52eeaa3ea77d24e26c2a2c3fdc4cb12f53a3
Chip Hill <hillkarl03@gmail.com>
2022-10-15 16:54:46 -0400

    re-styled main page and header for mobile first and some responsiveness

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,13 @@
 import React from "react";
+import logo from "../img/ds-logo.jpg"
 
 const Header = () => {
 
   return (
-    <header className="d-flex flex-wrap flex-row justify-content around bg-black">
+    <header className="d-flex flex-wrap justify-content-between bg-black">
+     <div className="ds-logo">
+      <img src={logo} alt="logo" />
+     </div>
       <ul id="header-nav" className="d-flex flex-wrap flex-row justify-content-evenly mx-auto p-2 w-50 text-light text-decoration-none">
         <li className="mx-2 list-inline-item">About</li>
         <li className="mx-2 list-inline-item">Events</li>
