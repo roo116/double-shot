448aeddbfba561319b951d62dcdfea9925dea0a3
Chip Hill <hillkarl03@gmail.com>
2022-10-27 14:21:55 -0400

    not working yet - gallery

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,29 +1,50 @@
 import React from "react";
-
+// import PhotoList from "./PhotoList";
 
 const Gallery = () => {
+  const photos = [
+    {
+      name: "/img/gallery/0-pic.jpg",
+      key:"0-pic.jpg"
+    },
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+  ];
 
-const photos = [
-  '0-pic.jpg', '1-pic.jpg','2-pic.jpg','3-pic.jpg','4-pic.jpg','5-pic.jpg','6-pic.jpg','7-pic.jpg','8-pic.jpg','9-pic.jpg','10-pic.jpg','11-pic.jpg','12-pic.jpg','13-pic.jpg','14-pic.jpg'
-]
-  
-return (
-    <div>
-      <section className="gallery-section d-flex flex-wrap justify-content-around">
-        <h1 id="Gallery">Gallery</h1>
-        {/* <p>{currentCategory.name}</p> */}
-        {photos.map((photo) => (
-        <img
-        src={`../img/gallery/${photo}`}
-        alt='band'
-        className="img-thumbnail mx-1"
-        key={photo.name}
-      />
-    ))}
-    </section>
-    </div>
+  photos.map((photo, i) => {console.log(photo[i].name)}
 
-  );
-};
 
-export default Gallery;
+//   return (
+//     <div>
+//       <section className="gallery-section d-flex flex-wrap justify-content-around">
+//         <h1 id="Gallery">Gallery</h1>
+//         <div>
+//       {photos.map((photo, i) => (
+//         <img
+//           src={photo[i].name}
+//           alt={photo[i].name}
+//           className="img-thumbnail mx-1"
+//           key={photo[i].name}
+//         />
+//       ))}
+//           </div>
+//       </section>
+
+
+
+//   );
+// };
+
+// export default Gallery;
---
+++ test.js
@@ -0,0 +1,24 @@
+const photos = [
+    {
+      name: "/img/gallery/0-pic.jpg",
+      key:"0-pic.jpg"
+    },
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+  ];
+
+  photos.map((photo, i) {
+    console.log(photo[i].name
+    }
