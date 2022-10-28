c848beba0097124214a70a0f1d89516546e5972a
roo116 <roo116@nc.rr.com>
2022-10-28 03:50:28 -0400

    working through the function to create the gallery.  Prototype working

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -2,49 +2,31 @@ import React from "react";
 // import PhotoList from "./PhotoList";
 
 const Gallery = () => {
-  const photos = [
-    {
-      name: "/img/gallery/0-pic.jpg",
-      key:"0-pic.jpg"
-    },
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-  ];
+  const photos = ["../img/gallery/0-pic.jpg"];
 
-  photos.map((photo, i) => {console.log(photo[i].name)}
+  return (
+    <section className="gallery-section d-flex flex-wrap justify-content-around">
+         <div>
+         <h1 id="Gallery">Gallery</h1>
+         </div>
+         <div>
+       {photos.map((photo, i) => (
+         <img
+           src={photo[i].name}
+           alt={photo[i].name}
+           className="img-thumbnail mx-1"
+           key={photo[i].name}
+         />
+       ))}
+ </div>
+       </section>
 
+  )
 
-//   return (
-//     <div>
-//       <section className="gallery-section d-flex flex-wrap justify-content-around">
-//         <h1 id="Gallery">Gallery</h1>
-//         <div>
-//       {photos.map((photo, i) => (
-//         <img
-//           src={photo[i].name}
-//           alt={photo[i].name}
-//           className="img-thumbnail mx-1"
-//           key={photo[i].name}
-//         />
-//       ))}
-//           </div>
-//       </section>
-
+//       
 
 
 //   );
-// };
+};
 
-// export default Gallery;
+export default Gallery;
--- test.js
+++ test.js
@@ -1,24 +1,16 @@
-const photos = [
-    {
-      name: "/img/gallery/0-pic.jpg",
-      key:"0-pic.jpg"
-    },
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-  ];
+// create a gallery
+// create the html page
+// create js file
+// function to get the HTML created to cycle though an array to pull the gallery files from the image directory
 
-  photos.map((photo, i) {
-    console.log(photo[i].name
-    }
+const photos = ["./src/img/gallery/0-pic.jpg", "./src/img/gallery/1-pic.jpg", "./src/img/gallery/2-pic.jpg"];
+
+
+
+for(let i=0; i<photos.length; i++) {
+  let html = `
+  <img src="${photos[i]}" alt="band" class="img-thumbnail mx1" /> 
+  `;
+  console.log(html)
+}
+
